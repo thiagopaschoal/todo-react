fdec5a9f06b88da21cbb1796cb87dfd757c4fac3
Thiago Sakurai Paschoal <thiagopaschoal076@gmail.com>
2021-03-06 17:28:38 -0300

    feat: update label to test deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deploy a SSG into S3 Bucket
         </a>
       </header>
     </div>
